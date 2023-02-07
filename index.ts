e5c611acc16e8b38d65f3ec1c40719073b5f818c
b035 <marlon@erler.cc>
2023-02-07 21:40:59 +0100

    cleanup

--- index.ts
+++ index.ts
@@ -7,17 +7,8 @@ export const USER_DIR = "users";
 
 async function main(subcommand: string, args: string[]) {
 	switch (subcommand) {
-		case "test": return await test();
-
 		default: throw ExitCodes.ErrNoCommand;
 	}
-}
-
-//TODO remove
-import * as Permissions from "./perm.js";
-async function test() {
-	const test_result = (await Permissions.check("5646-1", "test")).or_panic();	
-	console.log(test_result.value);
 }
 
 start_service(main).then((result: any) => console.log(result));
